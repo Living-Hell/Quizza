af5120022f1c3ec34f43de950c7c022a7f7dae39
Shushant Kumar <s.living.hell@gmail.com>
2023-04-29 21:51:30 +0530

    Added and styled the end page

---
+++ end.js
@@ -0,0 +1,15 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    // console.log(username.value);
+    saveScoreBtn.disabled = !username.value;
+})
+
+saveHighScore = e =>{
+    // console.log("Clicked save button!");
+    e.preventDefault();
+}
