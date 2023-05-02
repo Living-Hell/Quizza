f27fce334ffae80df7408f0041ac0f30580e265b
Shushant Kumar <s.living.hell@gmail.com>
2023-05-02 11:16:40 +0530

    Saving the high scores in local storage and added the Highscores page

--- end.js
+++ end.js
@@ -2,6 +2,13 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+// localStorage.setItem('highScores',JSON.stringify([]));
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+// console.log(highScores);
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,4 +19,19 @@ username.addEventListener("keyup", () => {
 saveHighScore = e =>{
     // console.log("Clicked save button!");
     e.preventDefault();
+
+    const score = {
+        score : mostRecentScore,
+        name : username.value
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+
+    // console.log(highScores);
+    // console.log(score); 
 }
---
+++ highscores.js
@@ -0,0 +1,5 @@
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+console.log(highScores);
+// console.log(highScoresList);
