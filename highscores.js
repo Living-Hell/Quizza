b47427422bdee70b051014660a8850ea6f40189f
Shushant Kumar <s.living.hell@gmail.com>
2023-05-02 11:32:23 +0530

    Completed the Highscores page to load and display High scores from local storage

--- highscores.js
+++ highscores.js
@@ -1,5 +1,11 @@
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-console.log(highScores);
+// console.log(highScores);
 // console.log(highScoresList);
+
+highScoresList.innerHTML = highScores
+    .map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
